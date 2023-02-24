7a176686dd6b30fdcbf000f3b25a7382a4e5f9ad
홍성민 <iamseongmin@naver.com>
2023-02-24 09:39:16 +0900

    소스트리 테스트

---
+++ d104/src/main/java/d104/d104/D104Application.java
@@ -0,0 +1,13 @@
+package d104.d104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D104Application.class, args);
+	}
+
+}
---
+++ d104/src/test/java/d104/d104/D104ApplicationTests.java
@@ -0,0 +1,13 @@
+package d104.d104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
